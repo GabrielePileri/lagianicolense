4e7cc1eec257226476b928587008f2dc53e33077
GabrielePileri <pilerigabriele1@gmail.com>
2025-01-02 12:39:22 +0100

    hero creation no image

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header/Header.tsx";
+import Hero from "./components/Hero/Hero.tsx";
 
 function App() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 }
