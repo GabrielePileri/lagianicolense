fc2a2367d5f945821ea026e21f89088668ca6bbc
GabrielePileri <pilerigabriele1@gmail.com>
2025-01-03 23:47:15 +0100

    centered but needs media query

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -6,6 +6,7 @@ function Hero() {
     <div className="hero">
       <div className="hero-content">
         <h1>Pizzeria La Gianicolense</h1>
+        <p className="p-hero">Ogni fetta una festa. Ogni piatto una scoperta</p>
       </div>
       <div className="hero-div">
         <img src={image} className="hero-img" />
