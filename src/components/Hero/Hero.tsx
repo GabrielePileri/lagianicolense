10f1c68c8192cc499f36c32159d64bf0a4857253
GabrielePileri <pilerigabriele1@gmail.com>
2025-01-03 11:22:14 +0100

    <img>

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,9 +1,16 @@
 import image from "./img/cinematic.jpg";
+import "./Hero.css";
 
 function Hero() {
   return (
     <div className="hero">
-      <img src={image} />
+      <div className="hero-content">
+        <h1>Pizzeria La Gianicolense</h1>
+      </div>
+      <div className="hero-div">
+        <img src={image} className="hero-img" />
+      </div>
+
     </div>
   );
 }
