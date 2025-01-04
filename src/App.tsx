8b9c0c2be72666df6f8761d38886f8e7e30599ca
GabrielePileri <pilerigabriele1@gmail.com>
2025-01-04 11:02:58 +0100

    la nostra storia

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header/Header.tsx";
 import Hero from "./components/Hero/Hero.tsx";
+import Story from "./components/Story/Story.tsx";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Story />
     </>
   );
 }
