5c1a25449b26dcdae6778efaa229ff9f3975eb46
GabrielePileri <pilerigabriele1@gmail.com>
2025-01-04 19:36:08 +0100

    gsap implementation

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,14 @@ import "./App.css";
 import Header from "./components/Header/Header.tsx";
 import Hero from "./components/Hero/Hero.tsx";
 import Story from "./components/Story/Story.tsx";
+import Pizze from "./components/Pizze/Pizze.tsx";
+
+import { gsap } from "gsap";
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 function App() {
   return (
@@ -9,6 +17,7 @@ function App() {
       <Header />
       <Hero />
       <Story />
+      <Pizze />
     </>
   );
 }
