a7309583b6c5f3fabcb1f1d4cf4d170ee78c787e
GabrielePileri <pilerigabriele1@gmail.com>
2025-01-07 12:47:15 +0100

    feedbacks

---
+++ src/components/Feedbacks/Feedbacks.tsx
@@ -0,0 +1,97 @@
+import "./Feedbacks.css";
+
+function Feedbacks() {
+  return (
+    <div className="feedbacks">
+      <section>
+        <div className="card">
+          <div className="card-header">
+            <div className="stelle">
+              <div className="stella"></div>
+              <div className="stella"></div>
+              <div className="stella"></div>
+              <div className="stella"></div>
+              <div className="stella"></div>
+              <h1 className="card-name">FULL NAME</h1>
+            </div>
+            <p className="card-text">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo,
+              quidem distinctio! Natus minima suscipit illo ea nam mollitia,
+              iure asperiores hic? Labore eos, amet aut dolorum officia odio
+              ducimus temporibus eligendi! Corporis suscipit voluptatem
+              consequuntur libero quam nostrum ex temporibus, delectus porro
+              mollitia voluptatum inventore ab quas possimus dignissimos id nisi
+              ducimus placeat, iste quis alias laboriosam eius. Ipsam laudantium
+              in porro accusamus. Dolorem asperiores quas rem? Voluptate at
+              error repudiandae esse sapiente nihil non dicta cumque omnis!
+              Minus vel quo, quasi fugiat vero, iure officiis est, sint ipsum
+              eum facere neque? Est debitis voluptatibus pariatur beatae tenetur
+              doloremque ut.
+            </p>
+          </div>
+          <div className="name">
+            <h1>Maps</h1>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="card">
+          <div className="card-header">
+            <div className="stelle">
+              <div className="stella"></div>
+              <div className="stella"></div>
+              <div className="stella"></div>
+              <div className="stella"></div>
+              <div className="stella"></div>
+              <h1 className="card-name">FULL NAME</h1>
+            </div>
+            <p className="card-text">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo,
+              quidem distinctio! Natus minima suscipit illo ea nam mollitia,
+              iure asperiores hic? Labore eos, amet aut dolorum officia odio
+              ducimus temporibus eligendi! Corporis suscipit voluptatem
+              consequuntur libero quam nostrum ex temporibus, delectus porro
+              mollitia voluptatum inventore ab quas possimus dignissimos id nisi
+              ducimus placeat, iste quis alias laboriosam eius. Ipsam laudantium
+              in porro accusamus. Dolorem asperiores quas rem? Voluptate at
+              error repudiandae esse sapiente nihil non dicta cumque omnis!
+              Minus vel quo, quasi fugiat vero, iure officiis est, sint ipsum
+              eum facere neque? Est debitis voluptatibus pariatur beatae tenetur
+              doloremque ut.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="card">
+          <div className="card-header">
+            <div className="stelle">
+              <div className="stella"></div>
+              <div className="stella"></div>
+              <div className="stella"></div>
+              <div className="stella"></div>
+              <div className="stella"></div>
+              <h1 className="card-name">FULL NAME</h1>
+            </div>
+            <p className="card-text">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo,
+              quidem distinctio! Natus minima suscipit illo ea nam mollitia,
+              iure asperiores hic? Labore eos, amet aut dolorum officia odio
+              ducimus temporibus eligendi! Corporis suscipit voluptatem
+              consequuntur libero quam nostrum ex temporibus, delectus porro
+              mollitia voluptatum inventore ab quas possimus dignissimos id nisi
+              ducimus placeat, iste quis alias laboriosam eius. Ipsam laudantium
+              in porro accusamus. Dolorem asperiores quas rem? Voluptate at
+              error repudiandae esse sapiente nihil non dicta cumque omnis!
+              Minus vel quo, quasi fugiat vero, iure officiis est, sint ipsum
+              eum facere neque? Est debitis voluptatibus pariatur beatae tenetur
+              doloremque ut.
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Feedbacks;
